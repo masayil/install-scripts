08079db068f9768743c8c9edbb61d5b825c99f72
masayil <857453455@qq.com>
2023-01-17 10:53:14 +0800

    manage redis by systemd notify

--- redis/install-redis.sh
+++ redis/install-redis.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 cd /opt/build-redis/redis-7.0.7
-make -j4 &> /dev/null && make install &> /dev/null
+make -j4 &> /dev/null && make USE_SYSTEMD=yes install &> /dev/null
 mkdir -p /var/log/redis && touch /var/log/redis/redis-server.log
 echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
 sysctl vm.overcommit_memory=1 &> /dev/null
